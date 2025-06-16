22087247c5ea9ad89395017757ea2c41259357bb
ljy2855 <ljy2855@naver.com>
2025-06-16 19:44:40 +0900

    fix: remove withCredentials

--- src/api/captchaApi.ts
+++ src/api/captchaApi.ts
@@ -14,9 +14,7 @@ export interface PredictResponse {
 const BASE_URL = 'http://43.200.200.176:8002'; // 백엔드 주소
 
 export async function fetchCaptchaChallenge(): Promise<CaptchaChallenge> {
-  const res = await axios.get<CaptchaChallenge>(`${BASE_URL}/captcha`, {
-    withCredentials: true,
-  });
+  const res = await axios.get<CaptchaChallenge>(`${BASE_URL}/captcha`);
   return res.data;
 }
 
@@ -29,9 +27,6 @@ export async function submitCaptchaImage(
     {
       image: imageBase64,
       id: id,
-    },
-    {
-      withCredentials: true,
     }
   );
 
