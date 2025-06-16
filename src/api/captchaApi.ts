a96b3d9ce4661f449ee1d65d30ceddaf57bf2fc2
ljy2855 <ljy2855@naver.com>
2025-06-16 19:28:24 +0900

    feat: add backend api host

--- src/api/captchaApi.ts
+++ src/api/captchaApi.ts
@@ -11,7 +11,7 @@ export interface PredictResponse {
   message: string;
 }
 
-const BASE_URL = 'http://localhost:8000'; // 백엔드 주소
+const BASE_URL = 'http://43.200.200.176:8002'; // 백엔드 주소
 
 export async function fetchCaptchaChallenge(): Promise<CaptchaChallenge> {
   const res = await axios.get<CaptchaChallenge>(`${BASE_URL}/captcha`, {
